888582502ebb73647fd4fabc3d4cb7c78b666da4
TheOnly8Z <888888zombies@gmail.com>
2023-09-17 02:00:16 -0700

    Rumination

---
+++ battle_move_scripts/SIF_PassiveRoundHealFromStatus.gd
@@ -0,0 +1,29 @@
+extends BattleMove
+
+export (Array, Resource) var required_status_effects:Array
+
+export (int, 0, 100) var chance:int = 100
+
+export (int) var duration_removed:int = 1
+export (int, 0, 100) var percent_hp_heal:int = 0
+export (int) var absolute_hp_heal:int = 0
+
+func notify(fighter, id:String, args):
+	if id == "round_ending" and fighter.status.hp < fighter.status.max_hp:
+		if required_status_effects.size() > 0 and effect_chance_succeeds(fighter, chance):
+			for effect_node in fighter.status.get_effects():
+				if required_status_effects.has(effect_node.effect):
+					var order = BattleOrder.new(fighter, BattleOrder.OrderType.FIGHT, self, {})
+					fighter.battle.queue_turn_action(order)
+					break
+
+	return .notify(fighter, id, args)
+
+func _execute(_battle, user, _argument, _attack_params):
+	if duration_removed > 0:
+		for effect_node in user.status.get_effects():
+			if required_status_effects.has(effect_node.effect):
+				effect_node.drain(duration_removed)
+				break
+	var hp = int(max(1, user.status.max_hp * percent_hp_heal / 100 + absolute_hp_heal))
+	user.get_controller().heal(hp)
--- battle_move_scripts/SIF_WallStatusMove.gd
+++ battle_move_scripts/SIF_WallStatusMove.gd
@@ -1,1 +1,79 @@
-extends "res://data/battle_move_scripts/WallStatusMove.gd"
+extends BattleMove
+
+export (Array, Resource) var wall_elemental_type:Array
+export (int) var amount:int = 3
+export (int) var sacrifice_hp_percent:int = 20
+
+export (Array, Resource) var status_effects:Array
+export (int, "All", "One At Random") var status_effects_to_apply:int = 0
+export (int, "User", "Target", "User and Target") var status_effects_recipient:int = 0
+export (int) var status_effect_amount:int = 3
+export (int, 0, 100) var status_effect_chance:int = 0
+
+export (Array, Resource) var fail_if_status_effect:Array
+
+func _execute(battle, user, _argument, attack_params):
+	var targets = attack_params.targets
+	if targets.size() == 0:
+		battle.queue_animate_turn_failed()
+		return 
+	
+	# If user has one of these status effects, the move fails.
+	if fail_if_status_effect.size() > 0:
+		for effect_node in user.status.get_effects():
+			if fail_if_status_effect.has(effect_node.effect):
+				battle.queue_animate_turn_failed()
+				return 
+	
+	launch_attack(battle, user, targets, attack_params, Bind.new(self, "contact"))
+
+func _is_unavoidable(user, target)->bool:
+	return ._is_unavoidable(user, target) or user.team == target.team
+
+func _get_damage_to_user(user)->int:
+	return user.status.max_hp * sacrifice_hp_percent / 100
+
+func get_decoy(user)->Decoy:
+	var types = get_types(user)
+	var type = preload("res://data/elemental_types/beast.tres")
+	if wall_elemental_type.size() > 0:
+		type = wall_elemental_type[0]
+	elif types.size() > 0:
+		type = types[0]
+	
+	if type.id == "metal":
+		var forms = user.get_species()
+		for form in forms:
+			if form is MonsterForm and form.move_tags.has("binvader"):
+				return preload("res://data/decoys/wall_binvader.tres")
+	
+	return load("res://data/decoys/wall_" + type.id + ".tres") as Decoy
+
+func contact(battle, user, target, _damage, attack_params):
+	var shield = WallStatus.new()
+	shield.set_decoy(get_decoy(user))
+	apply_status_effect(target, shield, amount)
+	
+	var recipients:Array
+	if status_effects_recipient == 0:
+		recipients = [user]
+	elif status_effects_recipient == 1:
+		recipients = [target]
+	else:
+		assert (status_effects_recipient == 2)
+		recipients = [user, target]
+
+	var status_effects = attack_params.get("status_effects", self.status_effects)
+	for target in recipients:
+		if status_effects_to_apply == 0:
+			for effect in status_effects:
+				apply_status_effect(target, effect, status_effect_amount)
+		else :
+			assert (status_effects_to_apply == 1)
+			var effect = battle.rand.choice(status_effects)
+			apply_status_effect(target, effect, status_effect_amount)
+
+func get_effect_hint(user, _target)->Array:
+	var status = WallStatus.new()
+	status.set_decoy(get_decoy(user))
+	return [status]
