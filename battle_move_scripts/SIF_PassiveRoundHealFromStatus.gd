19524ead66d5b80e26c9766eef2c83951018b837
TheOnly8Z <888888zombies@gmail.com>
2023-09-22 21:50:19 -0700

    heal per stack

--- battle_move_scripts/SIF_PassiveRoundHealFromStatus.gd
+++ battle_move_scripts/SIF_PassiveRoundHealFromStatus.gd
@@ -20,10 +20,14 @@ func notify(fighter, id:String, args):
 	return .notify(fighter, id, args)
 
 func _execute(_battle, user, _argument, _attack_params):
+	var stack = 0
 	if duration_removed > 0:
 		for effect_node in user.status.get_effects():
 			if required_status_effects.has(effect_node.effect):
+				stack += min(effect_node.get_duration(), duration_removed)
 				effect_node.drain(duration_removed)
-				break
-	var hp = int(max(1, user.status.max_hp * percent_hp_heal / 100 + absolute_hp_heal))
+	else:
+		stack = 1
+
+	var hp = int(max(1, stack * (user.status.max_hp * percent_hp_heal / 100 + absolute_hp_heal)))
 	user.get_controller().heal(hp)
